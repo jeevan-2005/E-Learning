0c435eafc86e8dbe09817428dd1741b1d983a508
jeevan-2005 <jeevanchoudhary2005@gmail.com>
2024-09-17 14:15:00 +0530

    created isAuth middleware and authorizeRoles middleware to authenticate and authorize user...

---
+++ lms_backend/src/middlewares/auth.ts
@@ -0,0 +1,51 @@
+require("dotenv").config();
+import { NextFunction, Request, Response } from "express";
+import catchAsyncError from "./catchAsyncError.middleware";
+import ErrorHandler from "../utils/ErrorHandler";
+import jwt, { JwtPayload } from "jsonwebtoken";
+import { redis } from "../utils/redis";
+
+export const isAuth = catchAsyncError(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const access_token = req.cookies.access_token as string;
+
+    if (!access_token) {
+      return next(
+        new ErrorHandler("Please login to access this resource", 400)
+      );
+    }
+
+    const decoded = jwt.verify(
+      access_token,
+      process.env.ACCESS_TOKEN_SECRET as string
+    ) as JwtPayload;
+
+    if (!decoded) {
+      return next(new ErrorHandler("Invalid access token", 400));
+    }
+
+    const user = await redis.get(decoded.id);
+
+    if (!user) {
+      return next(new ErrorHandler("User not found", 400));
+    }
+
+    req.user = JSON.parse(user);
+    next();
+  }
+);
+
+// authorize user based on roles
+export const authorizeRoles = (...roles: string[]) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    if (!req.user?.role) {
+      return next(
+        new ErrorHandler(
+          `Role: ${req.user?.role} is not allowed to access this resource.`,
+          403
+        )
+      );
+    }
+    next();
+  };
+};
