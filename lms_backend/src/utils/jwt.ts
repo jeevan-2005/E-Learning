0b4b968021c7bbef7fb9ef04946af325ceb3ef24
jeevan-2005 <jeevanchoudhary2005@gmail.com>
2024-09-17 14:10:47 +0530

    created cookies for access and refresh jwt tokens...

---
+++ lms_backend/src/utils/jwt.ts
@@ -0,0 +1,64 @@
+import { Response } from "express";
+import { IUser } from "../models/user.model";
+import { redis } from "./redis";
+
+require("dotenv").config();
+
+interface ITokenOptions {
+  expires: Date;
+  maxAge: number;
+  httpOnly: boolean;
+  sameSite: "lax" | "strict" | "none" | undefined;
+  secure?: boolean;
+}
+
+// parse environment variable to integrate with fallback value
+const accessTokenExpire = parseInt(
+  process.env.ACCESS_TOKEN_EXPIRE || "300",
+  10
+);
+const refreshTokenExpire = parseInt(
+  process.env.REFRESH_TOKEN_EXPIRE || "1200",
+  10
+);
+
+// options for cookies
+export const accessTokenOptions: ITokenOptions = {
+  expires: new Date(Date.now() + accessTokenExpire * 60 * 1000),
+  maxAge: accessTokenExpire * 60 * 1000,
+  httpOnly: true,
+  sameSite: "lax",
+};
+export const refreshTokenOptions: ITokenOptions = {
+  expires: new Date(Date.now() + refreshTokenExpire * 24 * 60 * 60 * 1000),
+  maxAge: refreshTokenExpire * 24 * 60 * 60 * 1000,
+  httpOnly: true,
+  sameSite: "lax",
+};
+
+const sendToken = async (user: IUser, res: Response, statusCode: number) => {
+  const accessToken = user.signAccessToken();
+  const refreshToken = user.signRefreshToken();
+  const userId = user?._id;
+
+  // upload sessions to redis
+  await redis.set(userId as string, JSON.stringify(user) as any);
+
+  // only set secure to true in production
+  if (process.env.NODE_ENV === "production") {
+    accessTokenOptions.secure = true;
+  }
+
+  res.cookie("access_token", accessToken, accessTokenOptions);
+  res.cookie("refresh_token", refreshToken, refreshTokenOptions);
+
+  // console.log("Cookies sent:", res.get('Set-Cookie'));
+
+  res.status(statusCode).json({
+    success: true,
+    user,
+    accessToken,
+  });
+};
+
+export default sendToken;
