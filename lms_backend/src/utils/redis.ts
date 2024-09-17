bad9197a0ca79b835b6fbe4c5f30f02e131c3b70
jeevan-2005 <jeevanchoudhary2005@gmail.com>
2024-09-17 14:05:57 +0530

    created connection with redis from upstash...

---
+++ lms_backend/src/utils/redis.ts
@@ -0,0 +1,16 @@
+require("dotenv").config();
+import { Redis } from "ioredis";
+
+const redisUrl = process.env.REDIS_URL as string;
+const redisOptions = {
+  connectTimeout: 30000, // Increase connection timeout to 30 seconds
+  retryStrategy: function (times) {
+    return Math.min(times * 50, 2000); // Retry strategy
+  },
+};
+
+export const redis = new Redis(redisUrl, redisOptions);
+
+redis.on("error", (error) => {
+  console.error("Redis client error:", error);
+});
