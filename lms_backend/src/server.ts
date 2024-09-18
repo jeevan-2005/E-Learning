c1b2445b83648665eb549630e6b85aae1ea092df
jeevan-2005 <jeevanchoudhary2005@gmail.com>
2024-09-18 10:30:53 +0530

    cloudinary connect done...

--- lms_backend/src/server.ts
+++ lms_backend/src/server.ts
@@ -5,7 +5,12 @@ import { v2 as cloudinary } from "cloudinary";
 
 const port = process.env.PORT;
 
-
+// cloudinary config
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_SECRET_KEY,
+});
 
 app.listen(port, () => {
   console.log(`Server is connected with port ${port}`);
