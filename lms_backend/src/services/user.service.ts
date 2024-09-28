3bc24e186ab442924354b2f99494683dfbf1152b
jeevan-2005 <jeevanchoudhary2005@gmail.com>
2024-09-28 17:52:36 +0530

    created update role service...

--- lms_backend/src/services/user.service.ts
+++ lms_backend/src/services/user.service.ts
@@ -12,3 +12,29 @@ export const getUserById = async (id: string, res: Response) => {
     });
   }
 };
+
+// get all users
+export const getAllUsersService = async (res: Response) => {
+  const users = await UserModel.find().sort({ createdAt: -1 });
+
+  res.status(201).json({
+    success: true,
+    users,
+  });
+};
+
+export const updateUserRoleService = async (
+  res: Response,
+  id: string,
+  role: String
+) => {
+  const updatedUser = await UserModel.findByIdAndUpdate(
+    id,
+    { role },
+    { new: true }
+  );
+  res.status(201).json({
+    success: true,
+    updatedUser,
+  });
+};
