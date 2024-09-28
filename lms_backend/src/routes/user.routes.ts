d2c012f744b5594bed4a0447067aa51799715586
jeevan-2005 <jeevanchoudhary2005@gmail.com>
2024-09-28 17:50:47 +0530

    created delete and update user role routes...

--- lms_backend/src/routes/user.routes.ts
+++ lms_backend/src/routes/user.routes.ts
@@ -1,17 +1,20 @@
 import express from "express";
 import {
+  deleteUser,
+  getAllUsers,
   getUserInfo,
   socialAuth,
   updateAccessToken,
   updatePasswrod,
   updateUserAvatar,
   updateUserInfo,
+  updateUserRole,
   userActivation,
   userLogin,
   userLogout,
   userRegister,
 } from "../controllers/user.controller";
-import { isAuth } from "../middlewares/auth";
+import { authorizeRoles, isAuth } from "../middlewares/auth";
 const userRouter = express.Router();
 
 //   /api/v1/user/
@@ -25,5 +28,8 @@ userRouter.post("/social-auth", socialAuth);
 userRouter.put("/update-user-info", isAuth, updateUserInfo);
 userRouter.put("/update-user-password", isAuth, updatePasswrod);
 userRouter.put("/update-user-avatar", isAuth, updateUserAvatar);
+userRouter.get("/get-users", isAuth, authorizeRoles("admin"), getAllUsers);
+userRouter.put("/update-user-role", isAuth, authorizeRoles("admin"), updateUserRole);
+userRouter.delete("/delete-user/:id", isAuth, authorizeRoles("admin"), deleteUser);
 
 export default userRouter;
