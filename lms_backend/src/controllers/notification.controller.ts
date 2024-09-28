0c9403725b7727a3c1484c5cc624b0a889248c1d
jeevan-2005 <jeevanchoudhary2005@gmail.com>
2024-09-28 17:55:52 +0530

    created get and update status controller...

---
+++ lms_backend/src/controllers/notification.controller.ts
@@ -0,0 +1,63 @@
+import NotificationModel from "../models/notification.model";
+import { Request, Response, NextFunction } from "express";
+import catchAsyncError from "../middlewares/catchAsyncError.middleware";
+import ErrorHandler from "../utils/ErrorHandler";
+import cron from "node-cron";
+
+const getAllNotifications = catchAsyncError(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const notifications = await NotificationModel.find().sort({
+        createdAt: -1,
+      });
+
+      if (!notifications) {
+        return next(new ErrorHandler("No notifications found", 404));
+      }
+
+      res.status(200).json({
+        success: true,
+        notifications,
+      });
+    } catch (error: any) {
+      return next(new ErrorHandler(error.message, 500));
+    }
+  }
+);
+
+const updateNotificationStatus = catchAsyncError(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const notification = await NotificationModel.findById(req.params.id);
+      if (!notification) {
+        return next(new ErrorHandler("Notification not found", 404));
+      }
+
+      notification.status = "read";
+
+      await notification.save();
+
+      const notifications = await NotificationModel.find().sort({
+        createdAt: -1,
+      });
+
+      res.status(200).json({
+        success: true,
+        notifications,
+      });
+    } catch (error: any) {
+      return next(new ErrorHandler(error.message, 500));
+    }
+  }
+);
+
+// deleting notification after 24 hours which are read and created 1 month ago.
+cron.schedule("0 0 0 * * *", async () => {
+  const thirtyDaysAgo = new Date(Date.now() - 30 * 24 * 60 * 60 * 1000);
+  await NotificationModel.deleteMany({
+    status: "read",
+    createdAt: { $lt: thirtyDaysAgo },
+  });
+});
+
+export { getAllNotifications, updateNotificationStatus };
