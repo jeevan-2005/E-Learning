08d1882a0b98fd39b2fc1897306521f4b19557d9
jeevan-2005 <jeevanchoudhary2005@gmail.com>
2024-09-28 17:54:23 +0530

    created get all courses service...

--- lms_backend/src/services/course.service.ts
+++ lms_backend/src/services/course.service.ts
@@ -1,14 +1,26 @@
 import { Response } from "express";
 import CourseModel from "../models/course.model";
 import catchAsyncError from "../middlewares/catchAsyncError.middleware";
+import { redis } from "../utils/redis";
 
 export const createCourse = catchAsyncError(
-    async (data: any, res: Response) => {
-        const course = await CourseModel.create(data);
+  async (data: any, res: Response) => {
+    const course = await CourseModel.create(data);
+    const allCourses = await CourseModel.find();
+    await redis.set("allCourses", JSON.stringify(allCourses));
+    res.status(201).json({
+      success: true,
+      course,
+    });
+  }
+);
 
-        res.status(201).json({
-            success: true,
-            course,
-        });
-    }
-)
+// get all courses
+export const getAllCoursesService = async (res: Response) => {
+  const courses = await CourseModel.find().sort({ createdAt: -1 });
+
+  res.status(201).json({
+    success: true,
+    courses,
+  });
+};
