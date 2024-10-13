5f92cfe8b06706750d3f11e06c9a7b9fc8f54871
jeevan-2005 <jeevanchoudhary2005@gmail.com>
2024-10-13 17:36:03 +0530

    created a function to give last 12Months data...

---
+++ lms_backend/src/utils/analyticsGenerator.ts
@@ -0,0 +1,44 @@
+import { Model } from "mongoose";
+
+interface MonthData {
+  month: string;
+  count: number;
+}
+
+export async function generateLast12MonthsData<T extends Document>(
+  model: Model<T>
+): Promise<{ last12Months: MonthData[] }> {
+  const last12Months: MonthData[] = [];
+  const currDate = new Date();
+  currDate.setDate(currDate.getDate() + 1);
+
+  for (let i = 11; i >= 0; i--) {
+    const endDate = new Date(
+      currDate.getFullYear(),
+      currDate.getMonth(),
+      currDate.getDate() - i * 28 // since every month has 28 days.
+    );
+    const startDate = new Date(
+      endDate.getFullYear(),
+      endDate.getMonth(),
+      endDate.getDate() - 28
+    );
+
+    const monthYear = endDate.toLocaleString("default", {
+      day: "numeric",
+      month: "short",
+      year: "numeric",
+    });
+
+    const documentCount = await model.countDocuments({
+      createdAt: {
+        $lte: endDate,
+        $gte: startDate,
+      },
+    });
+
+    last12Months.push({ month: monthYear, count: documentCount });
+  }
+
+  return { last12Months };
+}
