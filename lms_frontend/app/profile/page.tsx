282c5cb315965b2e4bb8baf71be311de4fe84b7b
jeevan-2005 <jeevanchoudhary2005@gmail.com>
2024-10-23 14:15:25 +0530

    profile page route created..

---
+++ lms_frontend/app/profile/page.tsx
@@ -0,0 +1,38 @@
+"use client";
+import React, { FC, useState } from "react";
+import Protected from "../hooks/useProtected";
+import Heading from "../utils/Heading";
+import Header from "../components/Header";
+import Profile from "../components/Profile/Profile";
+import { useSelector } from "react-redux";
+
+type Props = {};
+
+const Page: FC<Props> = (props) => {
+  const [open, setOpen] = useState(false);
+  const [activeItem, setActiveItem] = useState(5);
+  const [route, setRoute] = useState("Login");
+  const {user} = useSelector((state: any) => state.auth);
+
+  return (
+    <div>
+      <Protected>
+        <Heading
+          title={`${user?.name} Profile`}
+          description="This is Platform for student to learn and get help with their courses from teachers."
+          keywords="E-Learning, Programming, MERN, Machine-Learning, React, Redux"
+        />
+        <Header
+          open={open}
+          setOpen={setOpen}
+          activeItem={activeItem}
+          route={route}
+          setRoute={setRoute}
+        />
+        <Profile user={user} />
+      </Protected>
+    </div>
+  );
+};
+
+export default Page;
