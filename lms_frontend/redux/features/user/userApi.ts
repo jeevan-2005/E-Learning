5fbd2160e327f1756796f7e0f03388728671e52d
jeevan-2005 <jeevanchoudhary2005@gmail.com>
2024-10-23 14:23:11 +0530

    created queries like updateProfile, update-avatar

---
+++ lms_frontend/redux/features/user/userApi.ts
@@ -0,0 +1,24 @@
+import { apiSlice } from "../api/apiSlice";
+
+export const userApi = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    updateAvatar: builder.mutation({
+      query: (avatar) => ({
+        url: "/user/update-user-avatar",
+        method: "PUT",
+        body: {avatar},
+        credentials: "include" as const,
+      }),
+    }),
+    editProfile: builder.mutation({
+      query: ({name}) => ({
+        url: "/user/update-user-info",
+        method: "PUT",
+        body: {name},
+        credentials: "include" as const,
+      }),
+    }),
+  }),
+});
+
+export const { useUpdateAvatarMutation, useEditProfileMutation } = userApi;
