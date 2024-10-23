965218cd14fae580a1084ae3ae72acd23e1c50f8
jeevan-2005 <jeevanchoudhary2005@gmail.com>
2024-10-23 14:22:39 +0530

    created queries like register, login, logout, activation...

---
+++ lms_frontend/redux/features/auth/authApi.ts
@@ -0,0 +1,121 @@
+import { apiSlice } from "../api/apiSlice";
+import { userLoggedIn, userLoggedOut, userRegistration } from "./authSlice";
+
+type RegistrationResponse = {
+  message: string;
+  activationToken: string;
+};
+
+type RegistrationData = {};
+
+export const authApi = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    // endpoints here
+    register: builder.mutation<RegistrationResponse, RegistrationData>({
+      query: (data) => ({
+        url: "/user/register",
+        method: "POST",
+        body: data,
+        credentials: "include" as const,
+      }),
+
+      async onQueryStarted(arg, { queryFulfilled, dispatch }) {
+        try {
+          const result = await queryFulfilled;
+          dispatch(
+            userRegistration({
+              token: result.data.activationToken,
+            })
+          );
+        } catch (error) {
+          if (error instanceof Error) {
+            console.log(error.message);
+          }
+        }
+      },
+    }),
+    activation: builder.mutation({
+      query: ({ activation_token, activation_code }) => ({
+        url: "/user/activate-user",
+        method: "POST",
+        body: { activation_token, activation_code },
+        credentials: "include" as const,
+      }),
+    }),
+    login: builder.mutation({
+      query: ({ email, password }) => ({
+        url: "/user/login",
+        method: "POST",
+        body: { email, password },
+        credentials: "include" as const,
+      }),
+
+      async onQueryStarted(arg, { queryFulfilled, dispatch }) {
+        try {
+          const result = await queryFulfilled;
+          dispatch(
+            userLoggedIn({
+              user: result.data.user,
+              accessToken: result.data.accessToken,
+            })
+          );
+        } catch (error) {
+          if (error instanceof Error) {
+            console.log(error.message);
+          }
+        }
+      },
+    }),
+    socialAuth: builder.mutation({
+      query: ({ email, name, avatar }) => ({
+        url: "/user/social-auth",
+        method: "POST",
+        body: { name, email, avatar },
+        credentials: "include" as const,
+      }),
+
+      async onQueryStarted(arg, { queryFulfilled, dispatch }) {
+        try {
+          const result = await queryFulfilled;
+
+          dispatch(
+            userLoggedIn({
+              user: result.data.user,
+              accessToken: result.data.accessToken,
+            })
+          );
+        } catch (error) {
+          if (error instanceof Error) {
+            console.log(error.message);
+          }
+        }
+      },
+    }),
+    logout: builder.query({
+      query: () => ({
+        url: "/user/logout",
+        method: "GET",
+        credentials: "include" as const,
+      }),
+
+      async onQueryStarted(arg, { queryFulfilled, dispatch }) {
+        try {
+          await queryFulfilled;
+          dispatch(userLoggedOut());
+        } catch (error) {
+          if (error instanceof Error) {
+            console.log(error.message);
+          }
+        }
+      },
+    }),
+  }),
+});
+
+export const {
+  useRegisterMutation,
+  useActivationMutation,
+  useLoginMutation,
+  useSocialAuthMutation,
+  useLogoutQuery,
+} = authApi;
