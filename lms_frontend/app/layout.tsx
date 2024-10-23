1f6950f11f9f4ef68e331119c829876c138edc02
jeevan-2005 <jeevanchoudhary2005@gmail.com>
2024-10-23 14:04:16 +0530

    created layout of the webpage...

---
+++ lms_frontend/app/layout.tsx
@@ -0,0 +1,60 @@
+"use client";
+import "./globals.css";
+import { Josefin_Sans, Poppins } from "next/font/google";
+import { ThemeProvider } from "./utils/ThemeProvider";
+import { Toaster } from "react-hot-toast";
+import { Providers } from "./Provider";
+import { SessionProvider } from "next-auth/react";
+import { useLoadUserQuery } from "@/redux/features/api/apiSlice";
+import Loader from "./components/Loader/Loader";
+
+const poppinsFont = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+  variable: "--font-Poppins",
+});
+
+const josefinSans = Josefin_Sans({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+  variable: "--font-Josefin",
+});
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${poppinsFont.variable} ${josefinSans.variable} !bg-white bg-no-repeat dark:bg-gradient-to-b dark:from-gray-900 dark:to-black duration-300`}
+      >
+        <Providers>
+          <SessionProvider>
+            <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+              <Custom>{children}</Custom>
+              <Toaster position="top-center" reverseOrder={false} />
+            </ThemeProvider>
+          </SessionProvider>
+        </Providers>
+      </body>
+    </html>
+  );
+}
+
+
+const Custom : React.FC<{children: React.ReactNode}> = ({children}) => {
+
+  const {isLoading} = useLoadUserQuery({});
+ console.log("in custom loader ", isLoading)
+  return (
+    <>
+      {
+        isLoading ? (
+          <Loader />
+        ) : <>{children}</>
+      }
+    </>
+  )
+}
