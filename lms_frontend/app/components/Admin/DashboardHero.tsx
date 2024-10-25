5ae724b070f47a0650a0b8de376bf903c2fcdd55
jeevan-2005 <jeevanchoudhary2005@gmail.com>
2024-10-25 23:13:43 +0530

    componet having dashboardHeader

---
+++ lms_frontend/app/components/Admin/DashboardHero.tsx
@@ -0,0 +1,14 @@
+import React, { FC } from 'react'
+import DashboardHeader from "./DashboardHeader"
+
+type Props = {}
+
+const DashboardHero:FC<Props> = (props) => {
+  return (
+    <div>
+        <DashboardHeader />
+    </div>
+  )
+}
+
+export default DashboardHero
