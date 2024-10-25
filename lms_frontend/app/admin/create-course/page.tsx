c981752dad0a2887ae206fc7c74089db9f5d7154
jeevan-2005 <jeevanchoudhary2005@gmail.com>
2024-10-25 23:11:15 +0530

    created create course route for admin to create new course

---
+++ lms_frontend/app/admin/create-course/page.tsx
@@ -0,0 +1,31 @@
+"use client";
+import React, { FC } from "react";
+import Heading from "../../utils/Heading";
+import AdminSidebar from "../../components/Admin/sidebar/AdminSidebar";
+import DashboardHero from "../../components/Admin/DashboardHero";
+import CreateCourse from "../../components/Admin/course/CreateCourse";
+
+type Props = {};
+
+const page: FC<Props> = (props) => {
+  return (
+    <div>
+      <Heading
+        title="E-Learning - Admin"
+        description="E-Learning Platform for student to learn and get help with their courses from teachers."
+        keywords="E-Learning, Programming, MERN, Machine-Learning, React, Redux"
+      />
+      <div className="w-full flex h-screen">
+        <div className="w-[20%]">
+          <AdminSidebar />
+        </div>
+        <div className="w-[80%]">
+          <DashboardHero />
+          <CreateCourse />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default page;
