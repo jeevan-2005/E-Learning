c73b5694fc1318cd5fa6288279240c9aad2b8b4b
jeevan-2005 <jeevanchoudhary2005@gmail.com>
2024-10-26 21:52:36 +0530

    created a style for button (btn2)..

--- lms_frontend/app/styles/styles.ts
+++ lms_frontend/app/styles/styles.ts
@@ -3,4 +3,5 @@ export const style = {
     label: "text-[16px] font-Poppins text-black dark:text-white",
     input: "w-full text-black dark:text-white bg-transparent border rounded h-[40px] px-2 outline-none mt-[10px] font-Poppins",
     btn: "flex flex-row justify-center items-center px-6 py-3 rounded-full cursor-pointer bg-[#2190ff] min-h-[45px] w-full text-[16px] font-Poppins font-semibold",
+    btn2: "w-full 800px:w-[180px] h-[40px] bg-[#37a39a] flex items-center justify-center text-center text-[#fff] rounded mt-8 cursor-pointer font-Josefin",
 }
