d71dc569dca500ced1f138049d45a4fd09ee647a
jeevan-2005 <jeevanchoudhary2005@gmail.com>
2024-10-28 16:49:46 +0530

    added updateAccessToken middleware before isAuth in order routes

--- lms_backend/src/routes/order.route.ts
+++ lms_backend/src/routes/order.route.ts
@@ -1,12 +1,14 @@
 import express from "express";
 import { createOrder, getAllOrders } from "../controllers/order.controller";
 import { authorizeRoles, isAuth } from "../middlewares/auth";
+import { updateAccessToken } from "../controllers/user.controller";
 
 const orderRouter = express.Router();
 
-orderRouter.post("/create-order", isAuth, createOrder);
+orderRouter.post("/create-order", updateAccessToken, isAuth, createOrder);
 orderRouter.get(
   "/get-all-orders",
+  updateAccessToken,
   isAuth,
   authorizeRoles("admin"),
   getAllOrders
