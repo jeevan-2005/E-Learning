f22a2c2a956e5f04353b2f9321412ab52735df80
jeevan-2005 <jeevanchoudhary2005@gmail.com>
2024-10-28 16:50:21 +0530

    added updateAccessToken middleware before isAuth in course routes

--- lms_backend/src/routes/course.routes.ts
+++ lms_backend/src/routes/course.routes.ts
@@ -13,41 +13,52 @@ import {
   uploadCourse,
 } from "../controllers/course.controller";
 import { authorizeRoles, isAuth } from "../middlewares/auth";
+import { updateAccessToken } from "../controllers/user.controller";
 
 const courseRouter = express.Router();
 
 courseRouter.post(
   "/create-course",
+  updateAccessToken,
   isAuth,
   authorizeRoles("admin"),
   uploadCourse
 );
 courseRouter.put(
   "/edit-course/:id",
+  updateAccessToken,
   isAuth,
   authorizeRoles("admin"),
   editCourse
 );
 courseRouter.get("/get-course/:id", getSingleCourseUnpaid);
 courseRouter.get("/get-courses", getAllCourses);
-courseRouter.get("/get-course-content/:id", isAuth, getSingleCoursePaid);
-courseRouter.put("/add-question", isAuth, addQuestion);
-courseRouter.put("/add-answer", isAuth, addAnswer);
-courseRouter.put("/add-review/:id", isAuth, addReview);
+courseRouter.get(
+  "/get-course-content/:id",
+  updateAccessToken,
+  isAuth,
+  getSingleCoursePaid
+);
+courseRouter.put("/add-question", updateAccessToken, isAuth, addQuestion);
+courseRouter.put("/add-answer", updateAccessToken, isAuth, addAnswer);
+courseRouter.put("/add-review/:id", updateAccessToken, isAuth, addReview);
 courseRouter.put(
   "/review-reply",
+  updateAccessToken,
   isAuth,
   authorizeRoles("admin"),
   addReviewReply
 );
 courseRouter.get(
   "/get-all-courses",
+  updateAccessToken,
   isAuth,
   authorizeRoles("admin"),
   getAllCoursesAdmin
 );
 courseRouter.delete(
   "/delete-course/:id",
+  updateAccessToken,
   isAuth,
   authorizeRoles("admin"),
   deleteCourse
