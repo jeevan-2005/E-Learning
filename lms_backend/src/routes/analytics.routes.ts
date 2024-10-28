a6266a99f4871f0f20cdc6de7b813dcb5551aa70
jeevan-2005 <jeevanchoudhary2005@gmail.com>
2024-10-28 16:50:49 +0530

    added updateAccessToken middleware before isAuth in analytics routes

--- lms_backend/src/routes/analytics.routes.ts
+++ lms_backend/src/routes/analytics.routes.ts
@@ -5,23 +5,27 @@ import {
   getOrdersAnalytics,
   getUserAnalytics,
 } from "../controllers/analytics.controller";
+import { updateAccessToken } from "../controllers/user.controller";
 
 const analyticsRouter = express.Router();
 
 analyticsRouter.get(
   "/get-user-analytics",
+  updateAccessToken,
   isAuth,
   authorizeRoles("admin"),
   getUserAnalytics
 );
 analyticsRouter.get(
   "/get-course-analytics",
+  updateAccessToken,
   isAuth,
   authorizeRoles("admin"),
   getCourseAnalytics
 );
 analyticsRouter.get(
   "/get-order-analytics",
+  updateAccessToken,
   isAuth,
   authorizeRoles("admin"),
   getOrdersAnalytics
