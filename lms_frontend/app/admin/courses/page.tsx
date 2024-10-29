56b54c9abe9bad392ca1a47c108310f9ba7614f9
jeevan-2005 <jeevanchoudhary2005@gmail.com>
2024-10-29 10:24:46 +0530

    route to display all active courses created..

---
+++ lms_frontend/app/admin/courses/page.tsx
@@ -0,0 +1,34 @@
+"use client";
+import React, { FC } from "react";
+import AdminProtected from "../../hooks/adminProtected";
+import AdminSidebar from "../../components/Admin/sidebar/AdminSidebar";
+import DashboardHero from "../../components/Admin/DashboardHero";
+import Heading from "../../utils/Heading";
+import AllCourses from "../../components/Admin/course/AllCourses";
+
+type Props = {};
+
+const Page: FC<Props> = (props) => {
+  return (
+    <div>
+      <AdminProtected>
+        <Heading
+          title="E-Learning - Admin Panel"
+          description="E-Learning Platform for student to learn and get help with their courses from teachers."
+          keywords="E-Learning, Programming, MERN, Machine-Learning, React, Redux"
+        />
+        <div className="w-full flex h-screen">
+          <div className="w-[20%]">
+            <AdminSidebar />
+          </div>
+          <div className="w-[80%]">
+            <DashboardHero />
+            <AllCourses />
+          </div>
+        </div>
+      </AdminProtected>
+    </div>
+  );
+};
+
+export default Page;
