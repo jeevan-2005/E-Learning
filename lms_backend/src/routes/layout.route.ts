ad767a7ba46df1c1151a903d3674de53714c4e55
jeevan-2005 <jeevanchoudhary2005@gmail.com>
2024-11-01 18:51:11 +0530

    modified getLayout route (added type param)

--- lms_backend/src/routes/layout.route.ts
+++ lms_backend/src/routes/layout.route.ts
@@ -23,6 +23,6 @@ layoutRouter.put(
   authorizeRoles("admin"),
   editLayout
 );
-layoutRouter.get("/get-layout", getLayoutByType);
+layoutRouter.get("/get-layout/:type", getLayoutByType);
 
 export default layoutRouter;
