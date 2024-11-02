dd0239299e2198c5ac8339409bfb8a1cb5c2ed09
jeevan-2005 <jeevanchoudhary2005@gmail.com>
2024-11-02 15:10:33 +0530

    route to display order analytics

---
+++ lms_frontend/app/admin/order-analytics/page.tsx
@@ -0,0 +1,35 @@
+ "use client";
+import React, { FC } from "react";
+import AdminProtected from "../../hooks/adminProtected";
+import AdminSidebar from "../../components/Admin/sidebar/AdminSidebar";
+import DashboardHero from "../../components/Admin/DashboardHero";
+import Heading from "../../utils/Heading";
+import OrderAnalytics from "../../components/Admin/analytics/OrderAnalytics";
+
+
+type Props = {};
+
+const Page: FC<Props> = (props) => {
+  return (
+    <div>
+      <AdminProtected>
+        <Heading
+          title="E-Learning - Admin Panel"
+          description="E-Learning Platform for student to learn and get help with their courses from teachers."
+          keywords="E-Learning, Programming, MERN, Machine-Learning, React, Redux"
+        />
+        <div className="w-full flex min-h-screen">
+          <div className="w-[20%]">
+            <AdminSidebar selectedNav="Order Analytics" />
+          </div>
+          <div className="w-[85%]">
+            <DashboardHero />
+            <OrderAnalytics />
+          </div>
+        </div>
+      </AdminProtected>
+    </div>
+  );
+};
+
+export default Page;
