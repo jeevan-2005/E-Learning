1aa12681a37d7f3805c3d664634dce4659758351
jeevan-2005 <jeevanchoudhary2005@gmail.com>
2024-11-02 15:12:43 +0530

    created user, course, order analytics query

---
+++ lms_frontend/redux/features/analytics/analyticsApi.ts
@@ -0,0 +1,33 @@
+import { apiSlice } from "../api/apiSlice";
+
+export const analyticsApi = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    getCourseAnalytics: builder.query({
+      query: () => ({
+        url: "/analytics/get-course-analytics",
+        method: "GET",
+        credentials: "include" as const,
+      }),
+    }),
+    getOrderAnalytics: builder.query({
+      query: () => ({
+        url: "/analytics/get-order-analytics",
+        method: "GET",
+        credentials: "include" as const,
+      }),
+    }),
+    getUserAnalytics: builder.query({
+      query: () => ({
+        url: "/analytics/get-user-analytics",
+        method: "GET",
+        credentials: "include" as const,
+      }),
+    }),
+  }),
+});
+
+export const {
+  useGetCourseAnalyticsQuery,
+  useGetUserAnalyticsQuery,
+  useGetOrderAnalyticsQuery,
+} = analyticsApi;
