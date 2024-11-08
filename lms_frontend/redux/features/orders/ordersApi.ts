5be8fdde553806dfc9d97ce2612b0339c9773864
jeevan-2005 <jeevanchoudhary2005@gmail.com>
2024-11-08 22:18:50 +0530

    created createOrder, createPayment mutations

--- lms_frontend/redux/features/orders/ordersApi.ts
+++ lms_frontend/redux/features/orders/ordersApi.ts
@@ -9,7 +9,35 @@ export const ordersApi = apiSlice.injectEndpoints({
         credentials: "include" as const,
       }),
     }),
+    getStripePublishabeKey: builder.query({
+      query: () => ({
+        url: "/order/payment/stripe-publishable-key",
+        method: "GET",
+        credentials: "include" as const,
+      }),
+    }),
+    createPayment: builder.mutation({
+      query: (amount) => ({
+        url: "/order/payment",
+        method: "POST",
+        body: { amount },
+        credentials: "include" as const,
+      }),
+    }),
+    createOrder: builder.mutation({
+      query: ({ courseId, payment_info }) => ({
+        url: "/order/create-order",
+        method: "POST",
+        body: { courseId, payment_info },
+        credentials: "include" as const,
+      }),
+    }),
   }),
 });
 
-export const { useGetAllOrdersQuery } = ordersApi;
+export const {
+  useGetAllOrdersQuery,
+  useGetStripePublishabeKeyQuery,
+  useCreatePaymentMutation,
+  useCreateOrderMutation,
+} = ordersApi;
