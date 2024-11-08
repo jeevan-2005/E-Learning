38a850b98f57d26f688a10ee59556ea5b272e2f6
jeevan-2005 <jeevanchoudhary2005@gmail.com>
2024-11-08 22:06:50 +0530

    courseContent page for purchased course users...

---
+++ lms_frontend/app/course-access/[id]/page.tsx
@@ -0,0 +1,39 @@
+"use client"
+import Loader from '../../components/Loader/Loader';
+import { useLoadUserQuery } from '../../../redux/features/api/apiSlice';
+import { redirect } from 'next/navigation';
+import React, { FC, useEffect } from 'react'
+import CourseContentPurchasedUser from "../../components/Course/CourseContentPurchasedUser";
+
+type Props = {
+  params: any;
+}
+
+const Page:FC<Props> = ({params}) => {
+  const id = params.id;
+
+  const {data, error, isLoading} = useLoadUserQuery(undefined, {});
+
+  useEffect(() => {
+    if(data){
+      console.log(data);
+      const isPurchased = data?.user?.courses.find((course: any) => course._id === id);
+      if(!isPurchased){
+        redirect("/");
+      }
+    }
+    if(error){
+      redirect("/");
+    }
+  },[data, error])
+
+  if(isLoading) return <Loader />
+
+  return (
+    <div>
+      <CourseContentPurchasedUser id={id} user={data?.user} />
+    </div>
+  )
+}
+
+export default Page
