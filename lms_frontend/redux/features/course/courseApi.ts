e467031a956d7f3c65c7f2898d843f58d9f8ed51
jeevan-2005 <jeevanchoudhary2005@gmail.com>
2024-11-08 22:18:12 +0530

    created addQuestion, addAnswer, addReview, addReviewReply mutations

--- lms_frontend/redux/features/course/courseApi.ts
+++ lms_frontend/redux/features/course/courseApi.ts
@@ -1,3 +1,4 @@
+import { METHODS } from "http";
 import { apiSlice } from "../api/apiSlice";
 
 export const courseApi = apiSlice.injectEndpoints({
@@ -39,6 +40,68 @@ export const courseApi = apiSlice.injectEndpoints({
         credentials: "include" as const,
       }),
     }),
+    getSingleCourseDetails: builder.query({
+      query: (id) => ({
+        url: `/course/get-course/${id}`,
+        method: "GET",
+        credentials: "include" as const,
+      }),
+    }),
+    getPurchasedCourseDetials: builder.query({
+      query: (id) => ({
+        url: `/course/get-course-content/${id}`,
+        method: "GET",
+        credentials: "include" as const,
+      }),
+    }),
+    addNewQuestion: builder.mutation({
+      query: ({ question, courseId, courseContentId }) => ({
+        url: `/course/add-question`,
+        method: "PUT",
+        body: {
+          question,
+          courseId,
+          courseContentId,
+        },
+        credentials: "include" as const,
+      }),
+    }),
+    addAnswerToQuestion: builder.mutation({
+      query: ({ questionId, answer, courseId, courseContentId }) => ({
+        url: `/course/add-answer`,
+        method: "PUT",
+        body: {
+          answer,
+          questionId,
+          courseId,
+          courseContentId,
+        },
+        credentials: "include" as const,
+      }),
+    }),
+    addReview: builder.mutation({
+      query: ({ courseId, review, rating }) => ({
+        url: `/course/add-review/${courseId}`,
+        method: "PUT",
+        body: {
+          review,
+          rating,
+        },
+        credentials: "include" as const,
+      }),
+    }),
+    addReviewReply: builder.mutation({
+      query: ({ adminReply, reviewId, courseId }) => ({
+        url: "/course/review-reply",
+        method: "PUT",
+        body: {
+          adminReply,
+          reviewId,
+          courseId,
+        },
+        credentials: "include" as const,
+      }),
+    }),
   }),
 });
 
@@ -48,4 +111,10 @@ export const {
   useDeleteCourseMutation,
   useEditCourseMutation,
   useGetAllCoursesUserQuery,
+  useGetSingleCourseDetailsQuery,
+  useGetPurchasedCourseDetialsQuery,
+  useAddNewQuestionMutation,
+  useAddAnswerToQuestionMutation,
+  useAddReviewMutation,
+  useAddReviewReplyMutation,
 } = courseApi;
