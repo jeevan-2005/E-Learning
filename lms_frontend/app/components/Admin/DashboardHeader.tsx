e1e112558efb9939349e320484682602c0fc52dd
jeevan-2005 <jeevanchoudhary2005@gmail.com>
2024-11-10 16:45:29 +0530

    made notifications dynamic...

--- lms_frontend/app/components/Admin/DashboardHeader.tsx
+++ lms_frontend/app/components/Admin/DashboardHeader.tsx
@@ -1,6 +1,17 @@
-import React, { FC } from "react";
+import React, { FC, useEffect, useState } from "react";
 import ThemeSwitcher from "../../utils/ThemeSwitcher";
 import { IoMdNotificationsOutline } from "react-icons/io";
+import socketIO from "socket.io-client";
+import {
+  useGetAllNotificationsQuery,
+  useUpdateNotificationStatusMutation,
+} from "../../../redux/features/notifications/notificationApi";
+import { format } from "timeago.js";
+
+const ENDPOINT = process.env.NEXT_PUBLIC_SOCKET_SERVER_URI || "";
+const socketId = socketIO(ENDPOINT, {
+  transports: ["websocket"],
+});
 
 type Props = {
   open: boolean;
@@ -8,6 +19,49 @@ type Props = {
 };
 
 const DashboardHeader: FC<Props> = ({ open, setOpen }) => {
+  const { data, isLoading, refetch } = useGetAllNotificationsQuery(undefined, {
+    refetchOnMountOrArgChange: true,
+  });
+  const [
+    updateNotificationStatus,
+    { isSuccess, isLoading: updateLoading, error },
+  ] = useUpdateNotificationStatusMutation();
+
+  const [notifications, setNotifications] = React.useState<any[]>();
+
+  const [audio] = useState(
+    new Audio(
+      "https://res.cloudinary.com/jeevan27/video/upload/v1731219484/preview_wyixxl.mp3"
+    )
+  );
+
+  const playerNotificationSound = () => {
+    audio.play();
+  };
+
+  useEffect(() => {
+    if (data) {
+      setNotifications(
+        data?.notifications.filter((item: any) => item.status === "unread")
+      );
+    }
+    if (isSuccess) {
+      refetch();
+    }
+    audio.load();
+  }, [data, isSuccess]);
+
+  useEffect(() => {
+    socketId.on("newNotification", (data) => {
+      refetch();
+      playerNotificationSound();
+    });
+  }, []);
+
+  const handleNotificationStatusChange = async (id) => {
+    await updateNotificationStatus(id);
+  };
+
   return (
     <div className="w-full flex items-center justify-end p-6 fixed top-1 right-0">
       <ThemeSwitcher />
@@ -17,48 +71,45 @@ const DashboardHeader: FC<Props> = ({ open, setOpen }) => {
       >
         <IoMdNotificationsOutline className="text-2xl cursor-pointer text-black dark:text-white" />
         <span className="absolute -top-2 -right-2 bg-[#3ccba0] rounded-full w-[20px] h-[20px] text-[12px] flex items-center justify-center text-white">
-          3
+          {notifications && notifications.length}
         </span>
       </div>
       {open && (
-        <div className="w-[350px] h-[50vh] dark:bg-[#111C43] bg-white shadow-xl absolute top-16 z-10 rounded">
+        <div className="w-[350px] h-[50vh] dark:bg-[#111C43] bg-white shadow-2xl absolute top-16 -z-10 rounded px-1 py-1 overflow-auto border-l border-t border-[#0000003d] dark:border-[#ffffff3d]">
           <h5 className="text-black dark:text-white text-center text-[20px] p-3 font-Poppins">
             Notifications
           </h5>
-          <div className="dark:bg-[#2d3a4ea1] bg-[#00000013] font-Poppins border-b dark:border-b-[#ffffff47] border-b-[#0000000f]">
-            <div className="w-full flex items-center justify-between p-2">
-              <p className="text-black dark:text-white">
-                New Question Received
-              </p>
-              <p className="text-black dark:text-white cursor-pointer">
-                Mark as read
-              </p>
-            </div>
-            <p className="px-2 text-black dark:text-white">
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Hic,
-              aliquam qui! Nam repellat quam dolorum, sint quidem porro enim
-            </p>
-            <p className="text-black dark:text-white text-[14px] p-2">
-              5 days ago
-            </p>
-          </div>
-          <div className="dark:bg-[#2d3a4ea1] bg-[#00000013] font-Poppins border-b dark:border-b-[#ffffff47] border-b-[#0000000f]">
-            <div className="w-full flex items-center justify-between p-2">
-              <p className="text-black dark:text-white">
-                New Question Received
-              </p>
-              <p className="text-black dark:text-white cursor-pointer">
-                Mark as read
-              </p>
-            </div>
-            <p className="px-2 text-black dark:text-white">
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Hic,
-              aliquam qui! Nam repellat quam dolorum, sint quidem porro enim
-            </p>
-            <p className="text-black dark:text-white text-[14px] p-2">
-              5 days ago
-            </p>
-          </div>
+          {
+            notifications && notifications.length === 0 && (
+              <div className="w-full h-[35vh] flex items-center justify-center">
+                <p>No Notifications !!</p>
+              </div>
+            )
+          }
+          {notifications &&
+            notifications.map((notification: any, index: number) => (
+              <div
+                key={index}
+                className="dark:bg-[#2d3a4ea1] bg-[#00000013] font-Poppins border-b dark:border-b-[#ffffff47] border-b-[#0000000f] mb-1 rounded"
+              >
+                <div className="w-full flex items-center justify-between p-1">
+                  <p className="text-black dark:text-white">
+                    {notification?.title}
+                  </p>
+                  <p className="dark:text-[#3ccba0] text-[#000000b3] cursor-pointer text-[12px]" 
+                    onClick={() => handleNotificationStatusChange(notification?._id)}
+                  >
+                    Mark as read
+                  </p>
+                </div>
+                <p className="px-2 text-black dark:text-white text-[14px]">
+                  {notification?.message}
+                </p>
+                <p className="text-[#0000008a] dark:text-[#ffffffb3] text-[12px] p-2">
+                  {format(notification?.createdAt)}
+                </p>
+              </div>
+            ))}
         </div>
       )}
     </div>
