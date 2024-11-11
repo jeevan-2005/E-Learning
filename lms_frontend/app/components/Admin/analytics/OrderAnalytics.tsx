c6852f315d795bc69229182d7d0a540f6be144b7
jeevan-2005 <jeevanchoudhary2005@gmail.com>
2024-11-11 16:54:18 +0530

    modified grid keys

--- lms_frontend/app/components/Admin/analytics/OrderAnalytics.tsx
+++ lms_frontend/app/components/Admin/analytics/OrderAnalytics.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useEffect } from "react";
+import React, { FC } from "react";
 import { useGetOrderAnalyticsQuery } from "../../../../redux/features/analytics/analyticsApi";
 import Loader from "../../Loader/Loader";
 import {
