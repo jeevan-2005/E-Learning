24eadd7b88aabc47c1528aff449056c27bd8139a
jeevan-2005 <jeevanchoudhary2005@gmail.com>
2024-11-11 16:45:48 +0530

    corrected types in few functions

--- lms_backend/src/utils/redis.ts
+++ lms_backend/src/utils/redis.ts
@@ -4,7 +4,7 @@ import { Redis } from "ioredis";
 const redisUrl = process.env.REDIS_URL as string;
 const redisOptions = {
   connectTimeout: 30000, // Increase connection timeout to 30 seconds
-  retryStrategy: function (times) {
+  retryStrategy: function (times: number) {
     return Math.min(times * 50, 2000); // Retry strategy
   },
 };
