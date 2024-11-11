f4f1c3671c13bb1e33cf769f2feb5fd9f1000908
jeevan-2005 <jeevanchoudhary2005@gmail.com>
2024-11-11 09:27:51 +0530

    Footer added to profile page..

--- lms_frontend/app/profile/page.tsx
+++ lms_frontend/app/profile/page.tsx
@@ -5,6 +5,7 @@ import Heading from "../utils/Heading";
 import Header from "../components/Header";
 import Profile from "../components/Profile/Profile";
 import { useSelector } from "react-redux";
+import Footer from "../components/Footer";
 
 type Props = {};
 
@@ -12,7 +13,7 @@ const Page: FC<Props> = (props) => {
   const [open, setOpen] = useState(false);
   const [activeItem, setActiveItem] = useState(5);
   const [route, setRoute] = useState("Login");
-  const {user} = useSelector((state: any) => state.auth);
+  const { user } = useSelector((state: any) => state.auth);
 
   return (
     <div>
@@ -22,14 +23,17 @@ const Page: FC<Props> = (props) => {
           description="This is Platform for student to learn and get help with their courses from teachers."
           keywords="E-Learning, Programming, MERN, Machine-Learning, React, Redux"
         />
-        <Header
-          open={open}
-          setOpen={setOpen}
-          activeItem={activeItem}
-          route={route}
-          setRoute={setRoute}
-        />
-        <Profile user={user} />
+        <div className="min-h-screen">
+          <Header
+            open={open}
+            setOpen={setOpen}
+            activeItem={activeItem}
+            route={route}
+            setRoute={setRoute}
+          />
+          <Profile user={user} />
+          <Footer />
+        </div>
       </Protected>
     </div>
   );
