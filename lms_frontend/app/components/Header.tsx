b6f6651c1383d9925f4c4fd8067d2c6834ba83e6
jeevan-2005 <jeevanchoudhary2005@gmail.com>
2024-11-22 10:53:54 +0530

    modified logout fuctionality and added a menu to profile pic..

--- lms_frontend/app/components/Header.tsx
+++ lms_frontend/app/components/Header.tsx
@@ -10,14 +10,25 @@ import SignUp from "../components/Auth/SignUp";
 import Verification from "./Auth/Verification";
 import Image from "next/image";
 import avatar from "../../public/assests/avatar.webp";
-import { useSession } from "next-auth/react";
+import { signOut, useSession } from "next-auth/react";
 import {
-  useLogoutQuery,
+  useLogoutMutation,
   useSocialAuthMutation,
 } from "../../redux/features/auth/authApi";
 import toast from "react-hot-toast";
 import { useLoadUserQuery } from "../../redux/features/api/apiSlice";
 import Loader from "./Loader/Loader";
+import {
+  Avatar,
+  Divider,
+  IconButton,
+  ListItemIcon,
+  Menu,
+  MenuItem,
+  Tooltip,
+} from "@mui/material";
+import { Logout, Settings } from "@mui/icons-material";
+import { useSelector } from "react-redux";
 
 type Props = {
   open: boolean;
@@ -30,22 +41,26 @@ type Props = {
 const Header: FC<Props> = ({ open, setOpen, activeItem, route, setRoute }) => {
   const [active, setActive] = useState(false);
   const [openSidebar, setOpenSidebar] = useState(false);
-  const {
-    data: userData,
-    isLoading,
-    refetch,
-  } = useLoadUserQuery(undefined, {
+  const { user } = useSelector((state: any) => state.auth);
+  const { isLoading, refetch } = useLoadUserQuery(undefined, {
     refetchOnMountOrArgChange: true,
   });
-  const [socialAuth, { isSuccess, error, isLoading: socialLoading }] = useSocialAuthMutation();
+  const [socialAuth, { isSuccess, error, isLoading: socialLoading }] =
+    useSocialAuthMutation();
   const { data } = useSession();
-  const [logout, setLogout] = useState(false);
-  const {} = useLogoutQuery(undefined, {
-    skip: !logout ? true : false,
-  });
+  const [ logout ] = useLogoutMutation({});
+
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const opn = Boolean(anchorEl);
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleCl = () => {
+    setAnchorEl(null);
+  };
 
   useEffect(() => {
-    if (!userData && data) {
+    if (!user && data) {
       const handleSocialAuth = async () => {
         await socialAuth({
           name: data?.user?.name,
@@ -55,17 +70,14 @@ const Header: FC<Props> = ({ open, setOpen, activeItem, route, setRoute }) => {
       };
       handleSocialAuth();
     }
-    if (data === null && !userData) {
-      setLogout(true);
-    }
-  }, [data, userData]);
+  }, [data, user]);
 
   useEffect(() => {
     if (isSuccess) {
       refetch();
       toast.success("Login successful");
     }
-    if(error && "data" in error) {
+    if (error && "data" in error) {
       const errorData = error.data as { message?: string };
       const errorMessage = errorData?.message || "An error occurred";
       toast.error(errorMessage);
@@ -87,14 +99,24 @@ const Header: FC<Props> = ({ open, setOpen, activeItem, route, setRoute }) => {
       setOpenSidebar(false);
     }
   };
-  
+
+  const logoutHandler = async () => {
+    try {
+      await logout(undefined);
+
+      await signOut();
+      toast.success("Logged out successfully");
+    } catch (error) {
+      console.error("Logout failed", error);
+      toast.error("Logout failed. Please try again.");
+    }
+  };
+
   if (isLoading || socialLoading) return <Loader />;
 
   return (
     <div className="w-full relative">
-      {active && (
-        <div className="h-[80px]"></div>
-      )}
+      {active && <div className="h-[80px]"></div>}
       <div
         className={`${
           active
@@ -124,23 +146,91 @@ const Header: FC<Props> = ({ open, setOpen, activeItem, route, setRoute }) => {
                 />
               </div>
               <div className="ml-3 800px:ml-0">
-                {userData ? (
-                  <Link href={"/profile"}>
-                    <Image
-                      className="w-[45px] rounded-full"
-                      width={45}
-                      height={45}
-                      src={
-                        userData?.user.avatar
-                          ? userData?.user.avatar.url
-                          : avatar
-                      }
-                      alt="avatar"
-                      style={{
-                        border: activeItem === 5 ? "2px solid #37a39a" : "none",
+                {user ? (
+                  <>
+                    <Tooltip title="Account settings">
+                      <IconButton
+                        id="basic-button"
+                        aria-controls={opn ? "basic-menu" : undefined}
+                        aria-haspopup="true"
+                        aria-expanded={opn ? "true" : undefined}
+                        onClick={handleClick}
+                      >
+                        <Image
+                          className="w-[45px] rounded-full"
+                          width={45}
+                          height={45}
+                          src={user.avatar ? user?.avatar.url : avatar}
+                          alt="avatar"
+                          style={{
+                            border:
+                              activeItem === 5 ? "2px solid #37a39a" : "none",
+                          }}
+                        />
+                      </IconButton>
+                    </Tooltip>
+                    <Menu
+                      anchorEl={anchorEl}
+                      id="account-menu"
+                      open={opn}
+                      onClose={handleCl}
+                      onClick={handleCl}
+                      slotProps={{
+                        paper: {
+                          elevation: 0,
+                          sx: {
+                            overflow: "visible",
+                            filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
+                            mt: 1.5,
+                            "& .MuiAvatar-root": {
+                              width: 32,
+                              height: 32,
+                              ml: -0.5,
+                              mr: 1,
+                            },
+                            "&::before": {
+                              content: '""',
+                              display: "block",
+                              position: "absolute",
+                              top: 0,
+                              right: 14,
+                              width: 10,
+                              height: 10,
+                              bgcolor: "background.paper",
+                              transform: "translateY(-50%) rotate(45deg)",
+                              zIndex: 0,
+                            },
+                          },
+                        },
                       }}
-                    />
-                  </Link>
+                      transformOrigin={{ horizontal: "right", vertical: "top" }}
+                      anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
+                    >
+                      <Link href="/profile">
+                        {" "}
+                        <MenuItem>
+                          <Avatar />
+                          Profile
+                        </MenuItem>
+                      </Link>
+                      {user?.role === "admin" && (
+                        <Link href="/admin">
+                          {" "}
+                          <MenuItem>
+                            <Settings className="mr-2 my-1 text-[gray]" />
+                            Admin DashBoard
+                          </MenuItem>
+                        </Link>
+                      )}
+                      <Divider />
+                      <MenuItem onClick={logoutHandler}>
+                        <ListItemIcon>
+                          <Logout fontSize="small" />
+                        </ListItemIcon>
+                        Logout
+                      </MenuItem>
+                    </Menu>
+                  </>
                 ) : (
                   <HiOutlineUserCircle
                     size={25}
